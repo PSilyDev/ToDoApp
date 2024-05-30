6e2c9b6ca1960cfcbe875590c4eb054d3f8ab85d
PSilyDev <codes.prakhar@gmail.com>
2024-05-30 11:28:20 +0530

    DB initialized

---
+++ backend/db.js
@@ -0,0 +1,24 @@
+// MONGOOSE
+
+// IMPORTS
+const mongoose = require('mongoose');
+const { Schema } = require('zod');
+require('dotenv').config()
+
+// step 1 - connect to mongodb
+mongoose.connect(process.env.DB_URL);
+
+// step 2 - define schemas
+const ToDoSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    completed: Boolean
+})
+
+// step 3 - create model for schemas
+const ToDo = mongoose.model('ToDo', ToDoSchema);
+
+// step 4 - export models
+module.exports = {
+    toDo: ToDo
+}
