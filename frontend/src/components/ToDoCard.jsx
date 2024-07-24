2437bfa4c601d5e6525badf3d1a5e319a537af7f
PSilyDev <codes.prakhar@gmail.com>
2024-07-24 22:30:39 +0530

    ToDoCard UI changes for amaller screens

--- frontend/src/components/ToDoCard.jsx
+++ frontend/src/components/ToDoCard.jsx
@@ -14,18 +14,18 @@ export default function ToDoCard({todo, date}) {
         <div className="text-white text-xl pb-8">
             {todo.description}
         </div>
-        <div className="flex justify-between pb-2">
+        <div className="flex justify-between">
             {
                 !todo.completed ?
-                (<div className="w-1/3 flex sm: invisible lg:visible">
+                (<div className="w-1/3 flex sm: hidden lg:flex">
                     <div>
                         <Toggle todo={todo} id={todo.id}/>
                     </div>
                     <div className="text-white text-lg pt-0.5 ml-2 sm: invisible lg:visible">In Progress</div>
                 </div>) : (<div className="grid grid-cols-8"></div>)
             }
-            <div className="flex items-center">
-                <button onClick={() => handleDelete(todo._id)} className="sm: invisible lg:visible">
+            <div className="flex items-center lg:justify-end sm: w-full justify-center">
+                <button onClick={() => handleDelete(todo._id)} className="sm: hidden lg:block">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-9 h-9 mr-2"><path fill="#f50000" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM184 232H328c13.3 0 24 10.7 24 24s-10.7 24-24 24H184c-13.3 0-24-10.7-24-24s10.7-24 24-24z"/></svg>
                 </button>
                 <button className="bg-lime-500 text-white text-xl rounded font-medium px-3 py-1 disabled:opacity-25" onClick={() => updateCompleted(todo._id)} disabled={todo.completed}>MARK COMPLETE</button>
