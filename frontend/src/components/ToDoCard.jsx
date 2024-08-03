d2df28a2c0ed4424a87d8c3f5dd5f9343aaf08fa
PSilyDev <codes.prakhar@gmail.com>
2024-08-03 22:07:28 +0530

    Popup event handler added

--- frontend/src/components/ToDoCard.jsx
+++ frontend/src/components/ToDoCard.jsx
@@ -1,6 +1,7 @@
 import { useContext } from "react";
 import { Toggle } from "./toggle/Toggle";
 import { LoginContext } from "../context/LoginContext";
+import { Modal } from "./modal/Modal";
 
 export default function ToDoCard({todo, date}) {
     const { handleDelete, handleOnChange, updateCompleted } = useContext(LoginContext);
@@ -29,6 +30,9 @@ export default function ToDoCard({todo, date}) {
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-9 h-9 mr-2 sm: hidden lg:block"><path fill="#f50000" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM184 232H328c13.3 0 24 10.7 24 24s-10.7 24-24 24H184c-13.3 0-24-10.7-24-24s10.7-24 24-24z"/></svg>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="w-9 h-9 mr-2 lg:hidden"><path fill="#ff0000" d="M64 32C28.7 32 0 60.7 0 96L0 416c0 35.3 28.7 64 64 64l320 0c35.3 0 64-28.7 64-64l0-320c0-35.3-28.7-64-64-64L64 32zm88 200l144 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-144 0c-13.3 0-24-10.7-24-24s10.7-24 24-24z"/></svg>
                 </button>
+                
+
+                
                 <button className="bg-lime-500 text-white text-xl rounded font-medium px-3 py-1 disabled:opacity-25" onClick={() => updateCompleted(todo._id)} disabled={todo.completed}>MARK COMPLETE</button>
             </div>
         </div>
--- frontend/src/components/modal/Modal.jsx
+++ frontend/src/components/modal/Modal.jsx
@@ -1,6 +1,12 @@
 export const Modal = (props) => {
 
-    return(
+    const handleClick = (selected) => {
+        props.getSelected(selected);
+        props.setTrigger(false);
+                
+    }
+
+    return (props.trigger) ? (
         <div className="h-full w-full bg-transparent flex justify-center items-center">
             <div className="w-2/6 h-2/6 rounded-lg bg-slate-800 relative">
                 
@@ -10,9 +16,11 @@ export const Modal = (props) => {
 
                 <div className="text-white text-2xl text-center mt-10">Do you wish to continue?</div>
                 
-                <button type="button" disabled={true} className="absolute w-24 h-10 text-white bg-gradient-to-r from-purple-500 via-purple-600 to-purple-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-purple-300 dark:focus:ring-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center right-0 bottom-0 mb-4 mr-4">continue</button>
+                <button type="button" className="absolute w-24 h-10 text-white bg-gradient-to-r from-red-500 via-red-600 to-red-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center right-0 bottom-0 mb-4 mr-4" onClick={() => handleClick("no")}>No</button>
+
+                <button type="button" className="absolute w-24 h-10 text-white bg-gradient-to-r from-green-500 via-green-600 to-green-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center right-0 bottom-0 mb-4 mr-32" onClick={() => handleClick("yes")}>Yes</button>
             </div>
-            
+           
         </div>
-    )
+    ) : "";
 }
